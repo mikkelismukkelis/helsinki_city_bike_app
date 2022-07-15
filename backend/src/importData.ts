b25f5fa78f255f25bd526774fa9d159d2e74be95
Mikko M <muhonen.mikko@gmail.com>
2022-07-15 10:10:56 +0300

    deleted big csv files etc

---
+++ backend/src/importData.ts
@@ -0,0 +1,60 @@
+import fs from 'fs'
+import path from 'path'
+import { parse } from 'csv-parse'
+
+//import directory
+const importDirectory = path.join(__dirname, 'data_import')
+
+// export const importJourneyData = fs.readdirSync(importDirectory).forEach((file) => {
+//   // lets skip imported foder
+//   if (file === 'imported') return
+
+//   console.log(file)
+
+//   const importFile = path.join(importDirectory, file)
+
+//   fs.readFileSync
+
+//   fs.createReadStream(importFile)
+//     .pipe(parse({ delimiter: ',', from_line: 2 }))
+//     .on('data', (row) => {
+//       console.log(row)
+//       // TODO: data import
+//     })
+//     .on('end', () => {
+//       console.log(`${importFile} imported`)
+//     })
+//     .on('error', (error) => {
+//       console.log(`Error in reading file: ${error.message}`)
+//     })
+// })
+
+//function that reads import directory,  loops through files and imports data to db
+export const importJourneyData = fs.readdir(importDirectory, (err, files) => {
+  // error in directory reading
+  if (err) return console.log('Error in scanning import directory: ' + err)
+
+  // If import directory has only "imported" folder (files size = 1) return
+  if (files.length === 1) return console.log('data_import directory did not contain any files...')
+
+  // looping through all files,
+  files.forEach((file) => {
+    // lets skip imported foder
+    if (file === 'imported') return
+
+    const importFile = path.join(importDirectory, file)
+
+    fs.createReadStream(importFile)
+      .pipe(parse({ delimiter: ',', from_line: 2 }))
+      .on('data', (row) => {
+        console.log(row)
+        // TODO: data import
+      })
+      .on('end', () => {
+        console.log(`${importFile} imported`)
+      })
+      .on('error', (error) => {
+        console.log(`Error in reading file: ${error.message}`)
+      })
+  })
+})
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 
 import journeyRoutes from './routes/journeyRoutes'
 
+import { importJourneyData } from './importData'
+
 const PORT = process.env.PORT || 3001
 
 const app = express()
@@ -14,3 +16,9 @@ app.use(express.static('./client'))
 app.use('/api', journeyRoutes)
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+
+try {
+  importJourneyData
+} catch (error) {
+  console.log(error)
+}
