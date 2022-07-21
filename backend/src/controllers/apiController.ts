43a15ee7c927815ba17536b40ce7af6041dc6f95
Mikko M <muhonen.mikko@gmail.com>
2022-07-21 12:10:46 +0300

    one station api refactored

--- backend/src/controllers/apiController.ts
+++ backend/src/controllers/apiController.ts
@@ -2,6 +2,21 @@ import { RequestHandler } from 'express'
 
 import { connectToDatabase } from '../database/database'
 
+// We can quey db with promise way through this
+const queryDb = (command: string) => {
+  const db = connectToDatabase()
+
+  return new Promise((resolve, reject) => {
+    db.all(command, (error: Error, result: any[]) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(result)
+      }
+    })
+  })
+}
+
 // GET ALL JOURNEYS
 export const getJourneys: RequestHandler = (req, res, _next) => {
   const db = connectToDatabase()
@@ -41,13 +56,54 @@ export const getStations: RequestHandler = (_req, res, _next) => {
   db.close()
 }
 
+// Get calculated values for single station view
+const getDepartureCount = async (stationId: string) => {
+  const returnValue = await queryDb(
+    `SELECT count(*) as departure_count FROM journey_data WHERE departure_station_id = '${stationId}'`
+  )
+  return returnValue as any[]
+}
+
+// Get calculated values for single station view
+const getReturnCount = async (stationId: string) => {
+  const returnValue = await queryDb(
+    `SELECT count(*) as return_count FROM journey_data WHERE return_station_id = '${stationId}'`
+  )
+  return returnValue as any[]
+}
+
+// Get calculated values for single station view
+const getDepartureAvgDistance = async (stationId: string) => {
+  const returnValue = await queryDb(
+    `SELECT avg(covered_distance_m) as average_distance_departure FROM journey_data WHERE departure_station_id = '${stationId}'`
+  )
+  return returnValue as any[]
+}
+
+// Get calculated values for single station view
+const getReturnAvgDistance = async (stationId: string) => {
+  const returnValue = await queryDb(
+    `SELECT avg(covered_distance_m) as average_distance_return FROM journey_data WHERE return_station_id = '${stationId}'`
+  )
+  return returnValue as any[]
+}
+
 // GET ONE STATION BY ID
 // FID,ID,Nimi,Namn,Name,Osoite,Adress,Kaupunki,Stad,Operaattor,Kapasiteet,x,y
-export const getStationById: RequestHandler = (req, res, _next) => {
+export const getStationById: RequestHandler = async (req, res, _next) => {
   const stationId = req.params.id
 
   const db = connectToDatabase()
 
+  const departureCount = await getDepartureCount(stationId)
+
+  const returnCount = await getReturnCount(stationId)
+
+  const departureAvgDistance = await getDepartureAvgDistance(stationId)
+
+  const returnAvgDistance = await getReturnAvgDistance(stationId)
+
+  // all station information and merge all calculated results, send to client
   const sql = `SELECT * FROM station_list WHERE ID=${stationId}`
 
   db.all(sql, (err, rows) => {
@@ -55,7 +111,9 @@ export const getStationById: RequestHandler = (req, res, _next) => {
       res.status(400).json({ error: err.message })
       return
     }
-    res.json(rows)
+
+    const mergeResults = [...rows, ...departureCount, ...returnCount, ...departureAvgDistance, ...returnAvgDistance]
+    res.json(mergeResults)
   })
 
   db.close()
